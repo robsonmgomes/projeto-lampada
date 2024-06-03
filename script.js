deb9847dc0fcf4c498c1f2ff21c73ea9669f7bb2
Robson Medeiros <robsonmedeirosgomes@hotmail.com>
2024-06-02 23:26:52 -0300

    update css e js

--- script.js
+++ script.js
@@ -6,15 +6,14 @@ let ligado = false;
 function ligar() {
   ligado = !ligado;  
   
-  if (ligado) {
-    lampOn.style.display = 'none';
-    lampOff.style.display = 'block'
-    btn.value = 'Ligar';
-  } else {
-    lampOn.style.display = 'block';
-    lampOff.style.display = 'none';
-    lampOn.style.filter = 'drop-shadow(10px -100px 70px yellow)';;
-    btn.value = 'Desligar';
-  }
-  
+    if (ligado) {
+      lampOn.style.display = 'none';
+      lampOff.style.display = 'block'
+      btn.value = 'Ligar';
+    }else{
+      lampOn.style.display = 'block';
+      lampOff.style.display = 'none';
+      lampOn.style.filter = 'drop-shadow(10px -100px 70px yellow)';;
+      btn.value = 'Desligar';
+    }
 }
